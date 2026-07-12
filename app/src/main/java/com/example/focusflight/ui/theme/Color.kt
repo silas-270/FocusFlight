110c1d74590d1ea84b64e0d97bd1601281e6f0b3
silas_270 <kammersi.to@gmail.com>
2026-07-12 20:38:28 +0200

    Enhance theme contrast for outdoor usability: darken Midnight background and brighten container and text colors

--- app/src/main/java/com/example/focusflight/ui/theme/Color.kt
+++ app/src/main/java/com/example/focusflight/ui/theme/Color.kt
@@ -2,13 +2,13 @@ package com.example.focusflight.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Midnight = Color(0xFF0A0E1A)
-val DeepNavy = Color(0xFF121829)
-val Slate = Color(0xFF1C2333)
+val Midnight = Color(0xFF05070F)   // Darkened to ink-black
+val DeepNavy = Color(0xFF1C253D)   // Brightened to rich indigo-navy
+val Slate = Color(0xFF2D3859)      // Brightened secondary containers
 val Amber = Color(0xFFF5A623)
 val SoftAmber = Color(0x40F5A623)
-val OffWhite = Color(0xFFECEEF3)
-val Haze = Color(0xFF6B7280)
-val Dim = Color(0xFF3D4555)
+val OffWhite = Color(0xFFF1F3F7)   // Lighter text
+val Haze = Color(0xFF8C9BB0)       // Lighter silver-grey text
+val Dim = Color(0xFF4E5D78)        // Lighter steel grey
 val Green = Color(0xFF34D399)
-val Border = Color(0xFF1E2636)
+val Border = Color(0xFF2C3954)     // Brightened borders
